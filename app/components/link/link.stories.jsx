a5e0f411757406065de415be37c49344d69b886f
picografix <picografix@gmail.com>
2025-01-11 11:04:49 +0530

    some personalization changes

--- app/components/link/link.stories.jsx
+++ app/components/link/link.stories.jsx
@@ -7,8 +7,8 @@ export default {
 
 export const Default = () => (
   <StoryContainer style={{ fontSize: 18 }}>
-    <Link href="https://hamishw.com">Primary link</Link>
-    <Link secondary href="https://hamishw.com">
+    <Link href="https://gauransh.uk">Primary link</Link>
+    <Link secondary href="https://gauransh.uk">
       Secondary link
     </Link>
   </StoryContainer>
