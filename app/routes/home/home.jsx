e05d36c5135896ebc59ae21e3ba00d74597151b1
picografix <picografix@gmail.com>
2025-01-11 10:56:51 +0530

    basemetad changed

--- app/routes/home/home.jsx
+++ app/routes/home/home.jsx
@@ -41,8 +41,8 @@ export const links = () => {
 
 export const meta = () => {
   return baseMeta({
-    title: 'Designer + Developer',
-    description: `Design portfolio of ${config.name} — a product designer working on web & mobile apps with a focus on motion, experience design, and accessibility.`,
+    title: 'engineer + prototyper',
+    description: `Blog portfolio of ${config.name} — an engineer exploring his place in tech, looking beyond the horizon.`,
   });
 };
 
